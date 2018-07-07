d767def79d818d057ca6b6d50c1c773016cfe248
Elia <eliafesa@gmail.com>
2018-07-07 13:50:05 +0200

    He añadido un comentario.

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,13 @@
+/**
+ * Comentarios añadidos para el ejercicio 16.
+ * @author Elia
+ * @version 1.0
+ */
+
+
+public class HolaMundo {
+	public static void main (String[] parametro) {
+		System.out.println ("Hola Mundo, soy Elia");
+	}
+
+}
